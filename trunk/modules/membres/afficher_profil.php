f87a1d5df3a6f84b4bdc05f40f0274cf85115073
webmaster.desbeaux <webmaster.desbeaux@feec22e8-cd34-dd1b-c641-4ddb0f277d50>
2011-02-28 14:38:10 +0000

    Ajout du module forum (vide) MAJ de afficher_profil.php

--- trunk/modules/membres/afficher_profil.php
+++ trunk/modules/membres/afficher_profil.php
@@ -1,30 +1,5 @@
 <?php
 // Pas de v&#233;rification de droits d'acc&#232;s n&#233;cessaire : tout le monde peut voir un profil utilisateur :)
-
-//test pour la classe tableau
-include_once CHEMIN_LIB.'table.php';
-
-$tab = new Table("test", 1, 2);
-$tab->add_en_tete("truc", "machin", "chose");
-//$tab->add_att_en_tete(1, "width=200px");
-$tab->add_ligne("<b>a</b>", "d", "c");
-$tab->add_ligne("b", "c", "a");
-$tab->add_ligne("c", "a", "b");
-$tab->add_ligne("d", "b", "d");
-$tab->caption("test");
-
-echo $tab;
-
-
-//test pour la classe list
-include_once CHEMIN_LIB.'liste.php';
-$list = new Liste('testtttt', 0);
-$list->add_value('test1');
-
-
-
-
-
 // Si le param&#232;tre id est manquant ou invalide
 if (empty($_GET['id']) or !is_numeric($_GET['id'])) {
 
